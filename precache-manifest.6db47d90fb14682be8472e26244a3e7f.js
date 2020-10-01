68e5e72dd171235a74e90eeebed1bdc35ce29f60
Taufik Nurhidayat <fiik346@gmail.com>
2020-10-01 07:13:43 +0700

    Updates

---
+++ precache-manifest.6db47d90fb14682be8472e26244a3e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67f71bb553f0a9e03229fb6133df161",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6deb0fed33e13e81add0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2c606c89c3680693e4a",
+    "url": "/static/js/2.de9d6b72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de9d6b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6deb0fed33e13e81add0",
+    "url": "/static/js/main.4f19b2d7.chunk.js"
+  },
+  {
+    "revision": "3de055d04a29e374bbc7",
+    "url": "/static/js/runtime-main.5b967eb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
