49487b0c65a56590e425a1319dbb74d73c89f825
fik346 <fiik346@gmail.com>
2020-11-03 13:02:22 +0700

    Change

---
+++ layouts/default.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="clipped"
+      temporary
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          dense
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="deep-purple accent-3"
+      dark
+      flat
+      hide-on-scroll
+    >
+      <v-app-bar-nav-icon class="d-lg-none" @click.stop="drawer = !drawer" />
+      <v-toolbar-title>
+        <nuxt-link to="/" class="white--text text-decoration-none">{{title}}</nuxt-link>
+      </v-toolbar-title>
+      <v-spacer />
+      <div class="d-none d-lg-flex" v-for="(item,i) in items" :key="i">
+
+      <v-btn v-bind:to="item.to" exact text class="mx-1">
+        {{item.title}}
+      </v-btn>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Novel Indo'
+    }
+  },
+}
+</script>
